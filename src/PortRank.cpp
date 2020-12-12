837f722560a9622ee5625f32f8049363512f804c
Suchir Kavi <suchirkavi@gmail.com>
2020-12-12 00:14:48 -0600

    PageRank done

--- src/PortRank.cpp
+++ src/PortRank.cpp
@@ -4,8 +4,9 @@
 #include <numeric>
 
 int main(int argc, char * argv[]) {
-  std::string airports = "data/airports_popular.dat"; // TODO: need to change to normal once done testing
+  std::string airports = "data/airports.dat"; // TODO: need to change to normal once done testing
   std::string routes = "data/routes.dat";
+  std::string iterations_str = "20";
   double d = 0.85;
 
   for (int i = 1; i < argc; i++) {
@@ -19,7 +20,14 @@ int main(int argc, char * argv[]) {
         routes = argv[i];
       }
     }
+    else if (std::string("-i").compare(argv[i]) == 0) {
+      if (++i < argc) {
+        iterations_str = argv[i];
+      }
+    }
   }
+
+  int iterations = std::stoi(iterations_str);
 
   std::cout << "Using " << airports << " for airports file and " << routes << " for routes file." << std::endl;
   class Route {
@@ -46,23 +54,30 @@ int main(int argc, char * argv[]) {
     noDupRoutes.erase(std::unique(noDupRoutes.begin(), noDupRoutes.end()), noDupRoutes.end());
 
     for (int dest : noDupRoutes) {
-      M[j * N + dest] = 1.0 / noDupRoutes.size();
+      M[j + dest * N] = 1.0 / noDupRoutes.size();
     }
   }
 
   std::vector<double> M_hat(M);
   
-  std::for_each(M_hat.begin(), M_hat.end(), [d, N](double val) -> double { return val * d + ((1.0 - d) / N); });
+  std::for_each(M_hat.begin(), M_hat.end(), [d, N](double & val) { val = val * d + ((1.0 - d) / N); });
   
   std::vector<double> R(N);
-  std::generate(R.begin(), R.end(), [](){return (rand() % 101) / 100;});
-  double inv_norm = 1.0 / sqrt(std::inner_product(R.cbegin(), R.cend(), R.cbegin(), 0.0));
-  std::for_each(R.begin(), R.end(), [inv_norm](double val) -> double { return val * inv_norm; });
+  std::generate(R.begin(), R.end(), []() -> double {return (double)(std::rand() % 101) / 100.0;});
+  double inv_norm = 1.0 / sqrt(std::inner_product(R.begin(), R.end(), R.begin(), 0.0));
+  std::for_each(R.begin(), R.end(), [inv_norm](double & val) { val *= inv_norm; });
+  
+  for (int iter = 0; iter < iterations; iter++) {
+    for (size_t i = 0; i < N; i++) {
+      R[i] = std::inner_product(R.cbegin(), R.cend(), M.cbegin() + i * N, 0.0);
+    }
+  }
 
+  /*
   std::cout << std::endl;
   for (size_t i = 0; i < N; i++) {
     for (size_t j = 0; j < N; j++) {
-      std::cout << std::setprecision(2) << std::fixed << M[j * N + i] << " ";
+      std::cout << std::setprecision(2) << std::fixed << M[j + i * N] << " ";
     }
     std::cout << std::endl;
   }
@@ -71,6 +86,25 @@ int main(int argc, char * argv[]) {
   for (size_t i = 0; i < N; i++) {
     std::cout << i << ": " << fg.airports[i].name << std::endl;
   }
+  */
+
+  std::cout << std::endl;
+
+
+  std::vector<std::pair<int, double>> ranks;
+  ranks.reserve(N);
+
+  for (size_t i = 0; i < N; i++) {
+    ranks.push_back(std::pair((int)i, R[i]));
+  }
+
+  std::sort(ranks.begin(), ranks.end(), [](auto lhs, auto rhs){return lhs.second > rhs.second;});
+  
+  for (auto AP : ranks) {
+    std::cout << fg.airports[AP.first].name << ": " << AP.second << std::endl;
+  }
+
+
 
   return 0;
 }
